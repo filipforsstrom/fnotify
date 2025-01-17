032fb108ec1a658f064233c523c5d86208eb62e4
Filip Forsström <83241499+filipforsstrom@users.noreply.github.com>
2025-01-17 13:51:39 +0100

    service

---
+++ fnotify.nix
@@ -0,0 +1,51 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.services.fnotify;
+in {
+  options = {
+    services.fnotify = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Enable the fnotify service.";
+      };
+
+      dir = mkOption {
+        type = types.str;
+        default = "/dev";
+        description = "Directory to watch.";
+      };
+
+      prefix = mkOption {
+        type = types.str;
+        default = "tty";
+        description = "Prefix for files to watch.";
+      };
+
+      events = mkOption {
+        type = types.str;
+        default = "chmod,create,remove,rename,write";
+        description = "Comma-separated list of events to watch.";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.fnotify = {
+      description = "Fnotify Service";
+      after = ["graphical.target"];
+      wantedBy = ["multi-user.target"];
+
+      serviceConfig = {
+        ExecStart = "${pkgs.fnotify}/bin/fnotify --dir ${cfg.dir} --prefix ${cfg.prefix} --event ${cfg.events}";
+        Restart = "always";
+        RestartSec = "5s";
+      };
+    };
+  };
+}
